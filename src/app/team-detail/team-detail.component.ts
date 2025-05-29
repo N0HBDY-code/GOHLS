fc6a0f12c840b6192c97a5e526297dd6872c8ad2
N0HBDY-code <christopher.m.dick@ndsu.edu>
2025-05-29 18:06:29 -0500

    Update team detail header with logo and name

--- src/app/team-detail/team-detail.component.ts
+++ src/app/team-detail/team-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RosterComponent } from '../roster/roster.component';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-team-detail',
@@ -10,10 +11,27 @@ import { RosterComponent } from '../roster/roster.component';
   templateUrl: './team-detail.component.html',
   styleUrls: ['./team-detail.component.css']
 })
-export class TeamDetailComponent {
+export class TeamDetailComponent implements OnInit {
   teamId: string;
+  teamName: string = '';
+  teamLogo: string = '';
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: Firestore
+  ) {
     this.teamId = this.route.snapshot.paramMap.get('id')!;
+  }
+
+  async ngOnInit() {
+    if (this.teamId) {
+      const teamRef = doc(this.firestore, `teams/${this.teamId}`);
+      const teamSnap = await getDoc(teamRef);
+      if (teamSnap.exists()) {
+        const data = teamSnap.data();
+        this.teamName = `${data['city']} ${data['mascot']}`;
+        this.teamLogo = data['logoUrl'] || '';
+      }
+    }
   }
 }
