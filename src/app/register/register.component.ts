29115c5380d3aa987c2a0573225526814709597c
N0HBDY-code <christopher.m.dick@ndsu.edu>
2025-05-29 21:49:31 -0500

    Update authentication components

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,52 +1,53 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService, UserInfo } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
   user: UserInfo = {
-    fname : '',
-    lname : '',
-    email : '',
-    password : '',
-  }
+    username: '',
+    email: '',
+    password: ''
+  };
 
-  private authService = inject(AuthService)
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
   register() {
-
-    if (this.user.fname == '') {
-      alert('Please enter your first name')
-      return;
-    }
-    if (this.user.lname == '') {
-      alert('Please enter your Last name')
+    if (this.user.username === '') {
+      alert('Please enter a username');
       return;
     }
 
-    if (this.user.email == '') {
+    if (this.user.email === '') {
       alert('Please enter email');
       return;
     }
 
-    if (this.user.password == '') {
-      alert('Please enter your password')
+    if (this.user.password === '') {
+      alert('Please enter your password');
       return;
     }
 
-    this.authService.register(this.user);
-    this.user = {
-    fname : '',
-    lname : '',
-    email : '',
-    password : '',
-    }
-
+    this.authService.register(this.user)
+      .then(() => {
+        this.router.navigate(['/dashboard']);
+        this.user = {
+          username: '',
+          email: '',
+          password: ''
+        };
+      })
+      .catch(err => {
+        console.error('Registration failed:', err);
+        alert('Registration failed: ' + err.message);
+      });
   }
 }
